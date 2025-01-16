4b7bac7a9a16f0de5aaeca3c5d2b3e6080d58a90
nicolamanfredipf <nicola.manfredi@peoplefirs.ws>
2025-01-16 15:29:18 +0100

    navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import Logo from "../logo/Logo";
 import Navbar from "../navbar/Navbar";
 import styles from "./Header.module.css";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <Logo />
-            <Navbar />
-        </header>
-    );
+  // Stato per gestire la visibilità della navbar
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+
+  // Funzione per togglare la visibilità
+  const toggleNavbar = () => {
+    setIsNavbarVisible((prevState) => !prevState);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <button className={styles["menu-button"]} onClick={toggleNavbar}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path
+            fill="currentColor"
+            d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+          />
+        </svg>
+      </button>
+      {/* Passa lo stato di visibilità come prop */}
+      <Navbar isVisible={isNavbarVisible} />
+    </header>
+  );
 }
 
 export default Header;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,32 +1,69 @@
 import { Link } from "react-router-dom";
 import Button from "../button/Button";
 import styles from "./Navbar.module.css";
+import { useState } from "react";
 
+function Navbar({ isVisible }) {
+  const [isSubMenuVisible, setIsSubMenuVisible] = useState(false);
 
-function Navbar() {
+  const toggleSubMenu = () => {
+    setIsSubMenuVisible((prevState) => !prevState);
+  };
 
-
-
-    return (
-        <>
-            <nav className={styles.nav}>
-                <button className={styles["menu-button"]}>
-                    <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path fill="currentColor" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
-                    </svg>
-                </button>
-                <ul className={styles["nav-list"]}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Service</Link></li>
-                    <li><Link to="/">Menu</Link></li>
-                    <li><Link to="/about">Pages</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/contact"><Button lable={'Book a Table'} /></Link></li>
-                </ul>
-            </nav>
-        </>
-    );
+  return (
+    <nav
+      className={`${styles["nav"]} ${
+        isVisible ? styles["visible"] : styles["hidden"]
+      }`}
+    >
+      <ul className={styles["nav-list"]}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/service">Service</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li className={styles["sublist-container"]}>
+          {/* Bottone per il toggle del submenu */}
+          <button
+            onClick={toggleSubMenu}
+            className={`${styles["pages-button"]}`}
+          >
+            Pages
+          </button>
+          <ul
+            className={`${styles["sub-list"]} ${
+              isSubMenuVisible ? styles["visible"] : styles["hidden"]
+            }`}
+          >
+            <li>
+              <Link to="/booking">Booking</Link>
+            </li>
+            <li>
+              <Link to="/our-team">Our Team</Link>
+            </li>
+            <li>
+              <Link to="/testimonial">Testimonial</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/booking">
+            <Button label={"Book a Table"} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
