32a3295c4ecc50ac62f41a9aa7b202d1584f313f
nicolamanfredipf <nicola.manfredi@peoplefirs.ws>
2025-01-16 11:06:22 +0100

    our team ok

--- src/pages/our-team/OurTeam.js
+++ src/pages/our-team/OurTeam.js
@@ -1,9 +1,16 @@
+import PageHeader from "../../components/page-header/PageHeader";
+import Section from "../../components/section/Section";
+import Card from "../../components/card/Card";
 
-function OurTeam(){
-    return(
-        <>
-        </>
-    );
+function OurTeam() {
+  return (
+    <>
+      <PageHeader title={"Our Team"} />
+      <Section sectionTitle="Team Members" sectionSubtitle="Our Master Chefs">
+        <Card />
+      </Section>
+    </>
+  );
 }
 
 export default OurTeam;
