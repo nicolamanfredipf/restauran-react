cf9aaa129eb4730fe7c886d15c8b9f22338baaa6
nicolamanfredipf <nicola.manfredi@peoplefirs.ws>
2025-01-16 17:50:54 +0100

    UPDATES

--- src/pages/testimonial/Testimonial.js
+++ src/pages/testimonial/Testimonial.js
@@ -1,14 +1,16 @@
 import PageHeader from "../../components/page-header/PageHeader";
 import Section from "../../components/section/Section";
 import TestimonialCard from "../../components/testimonail-card/TestimonialCard";
-
+import styles from "./Testimonial.module.css";
 
 function Testimonial() {
   return (
     <>
       <PageHeader title={"Testimonial"} />
       <Section sectionTitle="Testimonail" sectionSubtitle="Our Clients Say!!!">
+        <div className={styles.carousel}>
         <TestimonialCard />
+        </div>
       </Section>
     </>
   );
