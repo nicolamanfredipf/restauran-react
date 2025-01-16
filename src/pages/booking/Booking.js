0388a0b44342fcda6548360327eabc861b6e850f
nicolamanfredipf <nicola.manfredi@peoplefirs.ws>
2025-01-16 11:03:28 +0100

    manca il video

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -1,9 +1,30 @@
+import PageHeader from "../../components/page-header/PageHeader";
+import Section from "../../components/section/Section";
+import styles from "./Booking.module.css";
 
-function Booking(){
-    return(
-        <>
-        </>
-    );
+function Booking() {
+  return (
+    <>
+      <PageHeader title={"Booking"} />
+      <aside>
+        <video></video>
+      </aside>
+      <Section sectionTitle="Reservation" sectionSubtitle="Book A Table Online">
+        <form className={styles["reservation-form"]}>
+            <input type="text" placeholder="Your Name" />
+            <input type="text" placeholder="Your Email" />
+            <input type="date" placeholder="Date & Time" />
+            <select>
+                <option>People 1</option>
+                <option>People 2</option>
+                <option>People 3</option>
+            </select>
+            <textarea placeholder="Special Request" />
+            <input type="submit" value="BOOK NOW" />
+        </form>
+      </Section>
+    </>
+  );
 }
 
 export default Booking;
