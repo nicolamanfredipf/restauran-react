60cc5d380097ff36b9f28636157bb08b5a754294
nicolamanfredipf <nicola.manfredi@peoplefirs.ws>
2025-01-16 11:35:29 +0100

    ok testimonial

--- src/pages/testimonial/Testimonial.js
+++ src/pages/testimonial/Testimonial.js
@@ -1,9 +1,17 @@
+import PageHeader from "../../components/page-header/PageHeader";
+import Section from "../../components/section/Section";
+import TestimonialCard from "../../components/testimonail-card/TestimonialCard";
 
-function Testimonial(){
-    return(
-        <>
-        </>
-    );
+
+function Testimonial() {
+  return (
+    <>
+      <PageHeader title={"Testimonial"} />
+      <Section sectionTitle="Testimonail" sectionSubtitle="Our Clients Say!!!">
+        <TestimonialCard />
+      </Section>
+    </>
+  );
 }
 
 export default Testimonial;
